ff169cabbcea826d31b874117e308c83bc6ecfed
Harshvardhan1012 <hs883532@gmail.com>
2025-11-24 18:00:52 +0530

    fix-aligment

--- src/sidebar/SidebarExample.tsx
+++ src/sidebar/SidebarExample.tsx
@@ -2,8 +2,10 @@ import { AlertDialogDemo } from '@/components/Alert/AlertDialog'
 import { DynamicSidebar } from '@/components/NavSideBar/DynamicSidebar'
 import { useAlert } from '@/components/services/toastService'
 import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { sidebarConfig } from './sidebarConfig'
+import { AuthContext } from '@/context/AuthContext'
 
 interface MainLayoutProps {
   children: React.ReactNode
@@ -11,7 +13,9 @@ interface MainLayoutProps {
 
 export function SidebarExample({ children }: MainLayoutProps) {
   const [open, setOpen] = useState(false)
+  const { loading } = useContext(AuthContext)
   const { showAlert } = useAlert()
+  const navigate = useNavigate()
 
   const handleConfirmLogout = () => {
     setOpen(false)
@@ -20,13 +24,16 @@ export function SidebarExample({ children }: MainLayoutProps) {
   }
 
   return (
-    <div className="flex h-full w-full rounded-sm">
-      <DynamicSidebar config={sidebarConfig}   />
-      <SidebarInset>
-        <nav className="flex items-center justify-between">
+    <div className="flex h-full w-full">
+      <DynamicSidebar
+        isLoading={loading}
+        config={sidebarConfig}
+      />
+      <SidebarInset className="flex-1 overflow-auto !m-0 !p-0 !rounded-none !shadow-none">
+        <nav className="flex items-center justify-between sticky top-0 z-10 bg-background border-b px-4 py-2">
           <SidebarTrigger className="mr-2" />
         </nav>
-        <div>{children}</div>
+        <div className="flex-1">{children}</div>
       </SidebarInset>
       {open && (
         <AlertDialogDemo
